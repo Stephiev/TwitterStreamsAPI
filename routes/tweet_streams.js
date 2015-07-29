5ea8a32e8dbca82f56bc5894f66c5198482ba4df
Stephanie Vasquez-Soltero <StephieVictoria@gmail.com>
2015-07-29 13:48:33 -0700

    Remove keys and clean up

--- routes/tweet_streams.js
+++ routes/tweet_streams.js
@@ -1,14 +1,14 @@
 "use strict"
 
 var Twitter = require('twitter');
-var Tweet = require("../models/tweetsM");
+var Tweet   = require("../models/tweetsM");
 
 // Insert your keys/secrets here
 var client = new Twitter ({
-  consumer_key: "dNHgUGwErdIqdTzKYF1GV8dpZ",
-  consumer_secret: "iWxXbTkjfDbVcu7PSFBl54tltz93AUaOzFL70KZ7GKlnY4XwLK",
-  access_token_key: "3247467584-xiMeSeKgCPtHH8A8zy2tLx9ucf7BVBrbueaDYQ8",
-  access_token_secret: "q846vWhn1bqQqFncSIMusQpJqtY4OXIprHWrn7qQdF0GD"
+  consumer_key:
+  consumer_secret:
+  access_token_key:
+  access_token_secret:
 });
 
 client.stream('statuses/filter', {track: "JavaScript Job"}, function(stream) {
@@ -17,11 +17,11 @@ client.stream('statuses/filter', {track: "JavaScript Job"}, function(stream) {
     console.log("NEW TWEET", tweet.text)
     var newTweet = new Tweet()
 
-    newTweet.created_at = tweet.created_at;
-    newTweet.text = tweet.text;
-    newTweet.name = tweet.user.name;
+    newTweet.created_at  = tweet.created_at;
+    newTweet.text        = tweet.text;
+    newTweet.name        = tweet.user.name;
     newTweet.screen_name = tweet.user.screen_name;
-    newTweet.location = tweet.user.location;
+    newTweet.location    = tweet.user.location;
 
     newTweet.save(function(err, data){
       if(err)
